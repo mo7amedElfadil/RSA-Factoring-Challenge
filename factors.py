091e4d9be25479ac14781a70e5ba7391369ea519
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-24 16:24:06 +0200

    trying venv

--- factors.py
+++ factors.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3.8
+import random
+from math import sqrt as sq
+import sys
+
 def is_prime(n):
     if n < 2:
         return False
@@ -7,13 +12,12 @@ def is_prime(n):
     return True
 
 def prime_factors_semiprime(semiprime):
-    from math import sqrt as sq
     prime_factors = []
     for i in range(2, int(sq(semiprime)) + 1):
         if semiprime % i == 0:
             prime_factors.append(i)
             prime_factors.append(semiprime // i)
-            break  
+            break
     return prime_factors
 
 
@@ -23,7 +27,6 @@ def gcd(a, b):
     return a
 
 def pollards_rho(n):
-    import random
     if n < 2:
         return None
     x = random.randint(2, n - 2)
@@ -45,10 +48,29 @@ def factorize_semiprime(semiprime):
     factor2 = semiprime // factor1
     return factor1, factor2
 
-def do_it():
-    import sys
-    f_name = sys.argv[1]
-    with open(f_name) as fp:
+def check_file():
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        exit()
+def read_file_all(f_name):
+    with open(f_name, "r") as fp:
+        file = fp.read()
+    file_content = file.splitlines()
+    for line in file_content:
+        try:
+            semiprime = int(line.strip())
+            if semiprime < 1000:
+                prime_factors = sorted(prime_factors_semiprime(semiprime))
+            else:
+                prime_factors = sorted(list(factorize_semiprime(semiprime)))
+            if len(prime_factors) == 2:
+                print("{:d}={:d}*{:d}".format(semiprime, prime_factors[1], prime_factors[0]))
+        except Exception as e:
+            print(f"Error processing line: {e}")
+
+
+def read_file_lines(f_name):
+    with open(f_name, "r") as fp:
         while True:
             prime_factors = []
             line = fp.readline()
@@ -56,16 +78,23 @@ def do_it():
                 break
             try:
                 semiprime = int(line.strip())
-                if semiprime < 10000:
-                    prime_factors = prime_factors_semiprime(semiprime)
+                if semiprime < 1000:
+                    prime_factors = sorted(prime_factors_semiprime(semiprime))
                 else:
-                    prime_factors = list(factorize_semiprime(semiprime))
+                    prime_factors = sorted(list(factorize_semiprime(semiprime)))
                 if len(prime_factors) == 2:
-                    print("{:d} = {:d} * {:d}".format(semiprime, prime_factors[1], prime_factors[0]))
+                    print("{:d}={:d}*{:d}".format(semiprime, prime_factors[1], prime_factors[0]))
             except Exception as e:
                 print(f"Error processing line: {e}")
 
+
+def do_it():
+    check_file()
+    f_name = sys.argv[1]
+    # read_file_all(f_name)
+    read_file_lines(f_name)
+
+
 if __name__ == "__main__":
-    import sys
     if (len(sys.argv) == 2):
-        do_it()    
+        do_it()
